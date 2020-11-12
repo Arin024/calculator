3c356da9b64847369a65dfd69034239a5cbc69e2
Arinze Christian <69147706+Arin024@users.noreply.github.com>
2020-11-12 00:41:32 -0800

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,11 @@
+//btn=document.querySelectorAll("button");
+
+function number(value){
+    document.form1.result.value+=value;
+}
+function cle(value){
+    document.form1.result.value=value;
+}
+function evalua(){
+    document.form1.result.value=eval(document.form1.result.value);
+}
